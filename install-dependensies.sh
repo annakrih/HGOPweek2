430dd9605629fbd758dda735d21b0bc67e7726ec
Anna Kristín <annah14@ru.is>
2017-12-09 03:07:35 +0000

    small change in install-dependensies - trying to debug

--- install-dependensies.sh
+++ install-dependensies.sh
@@ -2,11 +2,12 @@
 echo '-- Installing nvm'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # loading nvm
 
 #installing nodejs
 echo '-- Installing node js' 
 env VERSION=`python tools/getnodeversion.py` make install DESTDIR=`nvm_version_path v$VERSION` PREFIX=""
+nvm install 6.9.1
 nvm use 6.9.1
 PATH=./node_modules/.bin:$PATH #adding to PATH
 
