690e6cb210681c5afc895ef53d339fa2f1555206
Anna Kristín <annah14@ru.is>
2017-12-11 10:33:48 +0000

    moving files around

--- provisioning/ec2-instance-check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while ! test -e 'ec2-init-done.markerfile'
-do
-    sleep 2
-done
--- provisioning/ec2-instance-settings.sh
+++
@@ -1,4 +0,0 @@
-INSTANCE_ID=$(cat ./ec2_instance/instance-id.txt)
-SECURITY_GROUP_ID=$(cat ./ec2_instance/security-group-id.txt)
-SECURITY_GROUP_NAME=$(cat ./ec2_instance/security-group-name.txt)
-INSTANCE_PUBLIC_NAME=$(cat ./ec2_instance/instance-public-name.txt)
