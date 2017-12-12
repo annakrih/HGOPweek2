ee5b2e56e8ed0ea5a817aaedbf16cc35a87262db
Anna Kristín <annah14@ru.is>
2017-12-12 15:06:16 +0000

    debugging

--- provisioning/docker-instance-init.sh
+++ provisioning/docker-instance-init.sh
@@ -2,6 +2,8 @@
 
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
+echo HELLO WORLD
+
 sudo yum -y update
 sudo yum -y install docker
 sudo pip install docker-compose
