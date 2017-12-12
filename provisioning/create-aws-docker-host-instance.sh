15942fc3253a37dd95117341cbc1794a4fb5013c
Anna Kristín <annah14@ru.is>
2017-12-12 10:22:52 +0000

    trying to fix deployment

--- provisioning/create-aws-docker-host-instance.sh
+++ provisioning/create-aws-docker-host-instance.sh
@@ -58,4 +58,5 @@ echo Using CIDR ${MY_CIDR} for access restrictions.
 set +e
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_CIDR}
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 80 --cidr ${MY_CIDR}
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 8080 --cidr ${MY_CIDR}
 
